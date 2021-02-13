fce6acdf49a3b73f4c646bf1de5a5aedcebb1a1c
Kadir <kbsenay@hotmail.com>
2021-02-13 22:59:06 +0300

    13 Şubat 2. Commit

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -19,6 +19,7 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        
         public IResult Add(Product product)
         {
             if (product.ProductName.Length<2)
